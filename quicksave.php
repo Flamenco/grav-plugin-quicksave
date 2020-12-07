1997f9ac965c7148d27d255a683d9884653523df
Steven Spungin <steven@spungin.tv>
2020-12-06 22:59:16 -0500

    Check for core dependency

--- quicksave.php
+++ quicksave.php
@@ -34,34 +34,38 @@ use Twelvetone\Common\ServiceManager;
  */
 class QuicksavePlugin extends Plugin
 {
-    public static function getSubscribedEvents()
-    {
-        return [
-            'onPluginsInitialized' => ['onPluginsInitialized', 0]
-        ];
-    }
+	public static function getSubscribedEvents()
+	{
+		return [
+			'onPluginsInitialized' => ['onPluginsInitialized', 0]
+		];
+	}
 
-    public function onPluginsInitialized()
-    {
-        if (!$this->isAdmin()) {
-            return;
-        }
+	public function onPluginsInitialized()
+	{
+		if (!isset($this->grav['core-service-util'])) {
+			return;
+		}
+
+		if (!$this->isAdmin()) {
+			return;
+		}
 
 		if (!$this->grav['core-service-util']->checkPluginDependencies($this)) {
-            return;
-        }
+			return;
+		}
 
 		$this->grav['core-service-util']->checkAllPluginDependencies();
 
-        $this->enable([
-            'onPageNotFound' => ['onPageNotFound', 1],
-            'onAdminTwigTemplatePaths' => ['onAdminTwigTemplatePaths', 0],
-        ]);
+		$this->enable([
+			'onPageNotFound' => ['onPageNotFound', 1],
+			'onAdminTwigTemplatePaths' => ['onAdminTwigTemplatePaths', 0],
+		]);
 
-        $manager = ServiceManager::getInstance();
-        $manager->registerService('action', [
-            'render' => function () {
-                return '
+		$manager = ServiceManager::getInstance();
+		$manager->registerService('action', [
+			'render' => function () {
+				return '
                  <span style="display:inline-block">
                     <form id="saveajax" style="display: inline-block">
                         <button class="button multiline">
@@ -70,147 +74,147 @@ class QuicksavePlugin extends Plugin
                     <div id="healthy_snackbar"></div>
                 </span>
                 ';
-            },
-            'isEnabled' => function ($context) {
-                return $context && $context->exists();
-            },
-            'scope' => ['page'],
-            'order' => 'last'
-        ]);
+			},
+			'isEnabled' => function ($context) {
+				return $context && $context->exists();
+			},
+			'scope' => ['page'],
+			'order' => 'last'
+		]);
 
-        $manager->registerService('asset', [
-            'type' => 'css',
-            'url' => "plugins://quicksave/admin/assets/healthy_snackbar.css",
-            'scope' => ['page'],
-            'order' => 'first'
-        ]);
+		$manager->registerService('asset', [
+			'type' => 'css',
+			'url' => "plugins://quicksave/admin/assets/healthy_snackbar.css",
+			'scope' => ['page'],
+			'order' => 'first'
+		]);
 
-        $manager->registerService('asset', [
-            'type' => 'javascript',
-            'url' => "plugins://quicksave/admin/assets/healthy_snackbar.js",
-            'scope' => ['page'],
-            'order' => 'first'
-        ]);
+		$manager->registerService('asset', [
+			'type' => 'javascript',
+			'url' => "plugins://quicksave/admin/assets/healthy_snackbar.js",
+			'scope' => ['page'],
+			'order' => 'first'
+		]);
 
-        $manager->registerService("asset", [
-            'type' => 'twig',
-            'url' => "quicksave_ajax.html.twig",
-            'scope' => ['page'],
-            'order' => 'last'
-        ]);
+		$manager->registerService("asset", [
+			'type' => 'twig',
+			'url' => "quicksave_ajax.html.twig",
+			'scope' => ['page'],
+			'order' => 'last'
+		]);
 
-        $manager->registerService("asset", [
-            'type' => 'css',
-            'url' => "plugins://quicksave/admin/assets/quicksave.css",
-            'scope' => ['page'],
-            'order' => 'last'
-        ]);
+		$manager->registerService("asset", [
+			'type' => 'css',
+			'url' => "plugins://quicksave/admin/assets/quicksave.css",
+			'scope' => ['page'],
+			'order' => 'last'
+		]);
 
-        if ($this->config->get('plugins.quicksave.enable_keyboard_shortcut', false)) {
-            $manager->registerService("asset", [
-                'type' => 'js',
-                'url' => "plugins://quicksave/admin/assets/quicksave_hotkey_s.js",
-                'scope' => ['page'],
-                'order' => 'first'
-            ]);
-        }
-        if ($this->config->get('plugins.quicksave.enable_keyboard_shortcut_a', false)) {
-            $manager->registerService("asset", [
-                'type' => 'js',
-                'url' => "plugins://quicksave/admin/assets/quicksave_hotkey_a.js",
-                'scope' => ['page'],
-                'order' => 'first'
-            ]);
-        }
+		if ($this->config->get('plugins.quicksave.enable_keyboard_shortcut', false)) {
+			$manager->registerService("asset", [
+				'type' => 'js',
+				'url' => "plugins://quicksave/admin/assets/quicksave_hotkey_s.js",
+				'scope' => ['page'],
+				'order' => 'first'
+			]);
+		}
+		if ($this->config->get('plugins.quicksave.enable_keyboard_shortcut_a', false)) {
+			$manager->registerService("asset", [
+				'type' => 'js',
+				'url' => "plugins://quicksave/admin/assets/quicksave_hotkey_a.js",
+				'scope' => ['page'],
+				'order' => 'first'
+			]);
+		}
 //        $manager->registerService("key", [
 //            'scope' => ['page'],
 //            'keys' => ['s'],
 //            'modifiers' => ['shift', 'meta'],
 //            'clientCallback' => 'alert("ok")',
 //        ]);
-    }
+	}
 
-    public function onAdminTwigTemplatePaths($event)
-    {
-        $event['paths'] = array_merge($event['paths'], [__DIR__ . '/admin/templates']);
-        return $event;
-    }
+	public function onAdminTwigTemplatePaths($event)
+	{
+		$event['paths'] = array_merge($event['paths'], [__DIR__ . '/admin/templates']);
+		return $event;
+	}
 
-    public function onPageNotFound($e)
-    {
-        //TODO should never happen, right?
-        if (!$this->isAdmin()) {
-            return;
-        }
+	public function onPageNotFound($e)
+	{
+		//TODO should never happen, right?
+		if (!$this->isAdmin()) {
+			return;
+		}
 
-        $route = $this->grav['admin']->location . "/" . $this->grav['admin']->route;
+		$route = $this->grav['admin']->location . "/" . $this->grav['admin']->route;
 
-        switch ($route) {
-            // AJAX content
-            case "save-content/":
-                $this->checkPermissions($route);
-                /*
-                 * payload is JSON with {route:"...", content:"..."}
-                 */
-                $rawData = file_get_contents("php://input");
-                $req = json_decode($rawData, true);
-                $route = $req['route'];
-                $content = $req['content'];
+		switch ($route) {
+			// AJAX content
+			case "save-content/":
+				$this->checkPermissions($route);
+				/*
+				 * payload is JSON with {route:"...", content:"..."}
+				 */
+				$rawData = file_get_contents("php://input");
+				$req = json_decode($rawData, true);
+				$route = $req['route'];
+				$content = $req['content'];
 
-                if ($route === "") {
-                    self::result("ERROR", "The route was not set.");
-                }
+				if ($route === "") {
+					self::result("ERROR", "The route was not set.");
+				}
 
-                // As of Grav 1.7, the pages are not present unless this is called
-                if (method_exists($this->grav['admin'], 'enablePages')) {
-                    $this->grav['admin']->enablePages();
-                }
-                $page = $this->grav['pages']->find($route, true);
-                if (!$page) {
-                    self::result("ERROR", "The target page '$route' was not found");
-                }
+				// As of Grav 1.7, the pages are not present unless this is called
+				if (method_exists($this->grav['admin'], 'enablePages')) {
+					$this->grav['admin']->enablePages();
+				}
+				$page = $this->grav['pages']->find($route, true);
+				if (!$page) {
+					self::result("ERROR", "The target page '$route' was not found");
+				}
 
-                $curContent = $page->rawMarkdown();
-                if (strcmp($content, $curContent) == 0) {
-                    self::result("OK", "The content has not changed.");
-                }
+				$curContent = $page->rawMarkdown();
+				if (strcmp($content, $curContent) == 0) {
+					self::result("OK", "The content has not changed.");
+				}
 
-                $page->content($content);
-                $page->save();
+				$page->content($content);
+				$page->save();
 				$this->grav['core-service-util']->save($page);
 
-                if ($this->grav['config']->get("plugins.quicksave.clear_dirty")) {
-                    //TODO implement clear_dirty
-                }
+				if ($this->grav['config']->get("plugins.quicksave.clear_dirty")) {
+					//TODO implement clear_dirty
+				}
 
-                self::result("OK", "The content was saved.");
-                break;
+				self::result("OK", "The content was saved.");
+				break;
 
-            default:
-                break;
-        }
-    }
+			default:
+				break;
+		}
+	}
 
-    /**
-     * @param $status "OK" or "ERROR"
-     * @param string $message
-     */
-    static function result($status, $message = "")
-    {
-        header('HTTP/1.1 200 OK');
-        $res = ["status" => $status, "message" => $message];
-        die(json_encode($res));
-    }
+	/**
+	 * @param $status "OK" or "ERROR"
+	 * @param string $message
+	 */
+	static function result($status, $message = "")
+	{
+		header('HTTP/1.1 200 OK');
+		$res = ["status" => $status, "message" => $message];
+		die(json_encode($res));
+	}
 
-    private function checkPermissions($route)
-    {
-        if (!$this->grav['admin']->authorize([
-            "admin.super",
-            "admin.pages",
-            "plugin.quicksave.content"
-        ])) {
-            self::result("ERROR", "Permission denied.");
-        }
-    }
+	private function checkPermissions($route)
+	{
+		if (!$this->grav['admin']->authorize([
+			"admin.super",
+			"admin.pages",
+			"plugin.quicksave.content"
+		])) {
+			self::result("ERROR", "Permission denied.");
+		}
+	}
 }
 
